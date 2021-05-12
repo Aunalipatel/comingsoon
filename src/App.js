d96a1c07a40bb13c410cc420b4b3195fa95c9e0d
Aunalipatel <69858342+Aunalipatel@users.noreply.github.com>
2021-05-12 19:04:30 +0530

    last - aun

--- src/App.js
+++ src/App.js
@@ -9,32 +9,33 @@ import Refundpolicy from './screens/static/Refund-policy';
 import TermsandConditions from './screens/static/TermsandConditions';
 import PrivacyPolicy from './screens/static/PrivacyPolicy';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavbarComponent from './components/NavbarComponent';
+
+import Amplify from 'aws-amplify';
+import Auth from './Auth';
+
+Amplify.configure({
+  Auth,
+});
 
 function App() {
   return (
-    
     <div className="App">
       <Router>
-        <div >
+        <div>
           <NavbarComponent />
-          
         </div>
         <Switch>
-
-            <Route path = "/" exact component = {HomeScreen2}/>
-            <Route path = "/aboutus" exact component = {Aboutus}/>
-            <Route path = "/contact-us" exact component = {ContactUs}/>
-            <Route path = "/refund-policy" exact component = {Refundpolicy}/>
-            <Route path = "/tandc" exact component = {TermsandConditions}/>
-            <Route path = "/privacy-policy" exact component = {PrivacyPolicy}/>
-            <Route path = "/payment" exact component = {PaymentScreen}/>
-          </Switch>
-        </Router>
-
-      
-
+          <Route path="/" exact component={HomeScreen2} />
+          <Route path="/aboutus" exact component={Aboutus} />
+          <Route path="/contact-us" exact component={ContactUs} />
+          <Route path="/refund-policy" exact component={Refundpolicy} />
+          <Route path="/tandc" exact component={TermsandConditions} />
+          <Route path="/privacy-policy" exact component={PrivacyPolicy} />
+          <Route path="/payment" exact component={PaymentScreen} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Auth.js
@@ -0,0 +1,15 @@
+const Auth = {
+  identityPoolId: 'us-east-2:5e20117e-f14e-433f-bffd-3937159b22ee',
+  region: 'us-east-2',
+  identityPoolRegion: 'us-east-2',
+  userPoolId: 'us-east-2_zcdpakGmj',
+  userPoolWebClientId: '737k0qeig9h38me9trniukr7tg',
+  oauth: {
+    domain: 'techguy.auth.us-east-2.amazoncognito.com/',
+    redirectSignIn: 'http://localhost:3000/',
+    redirectSignOut: 'http://localhost:3000/',
+    responseType: 'token',
+  },
+};
+
+export default Auth;
